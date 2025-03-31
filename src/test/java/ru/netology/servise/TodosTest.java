7fca220ab7b5aeee6498a267b4c282bb8a4347ca
Asja <anastasjamedved@gmail.com>
2025-03-31 20:41:44 +0300

    pull commit

---
+++ src/test/java/ru/netology/servise/TodosTest.java
@@ -0,0 +1,48 @@
+package ru.netology.servise;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TodosTest {
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = { simpleTask, epic, meeting };
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldSearchTasksByQuery() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+        Meeting meeting = new Meeting(555, "Выкатка версии", "НетоБанк", "Вторник");
+
+        Todos todos = new Todos();
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] result = todos.search("НетоБанк");
+
+        Task[] expected = {meeting};
+        Assertions.assertArrayEquals(expected, result);
+    }
+}
